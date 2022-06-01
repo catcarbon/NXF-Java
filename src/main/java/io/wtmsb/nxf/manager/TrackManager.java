3684aff3cf5a9a286244ce41810babe292b996f0
Yi Zhang <z@wtmsb.io>
2022-05-31 23:08:21 -0400

    update TrackManager.java

--- src/main/java/io/wtmsb/nxf/manager/TrackManager.java
+++ src/main/java/io/wtmsb/nxf/manager/TrackManager.java
@@ -2,21 +2,19 @@ package io.wtmsb.nxf.manager;
 
 import com.google.common.collect.LinkedListMultimap;
 import com.google.common.collect.ListMultimap;
-import com.google.protobuf.ByteString;
-import io.wtmsb.nxf.domain.FlightData;
 import io.wtmsb.nxf.domain.RadarTarget;
 import io.wtmsb.nxf.domain.Track;
 import io.wtmsb.nxf.utility.GeoCalculator;
-import io.wtmsb.nxf.validation.IcaoAddress;
+import lombok.NonNull;
 import lombok.Synchronized;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import java.time.Duration;
+import java.time.Instant;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 import static io.wtmsb.nxf.domain.IRadarComponent.TransponderMode;
 
@@ -25,8 +23,17 @@ public class TrackManager {
 	private ControllerManager controllerManager;
 	private final Map<RadarTarget, Track> targetTrackMap;
 	private final ListMultimap<Track, RadarTarget> trackTargetMultiMap;
+	private final Map<String, Track> trackByCallsignMap;
 
 	private final int radarReturnCount = 6;
+
+	{
+		targetTrackMap = new ConcurrentHashMap<>();
+		trackTargetMultiMap = LinkedListMultimap.create();
+		trackByCallsignMap = new ConcurrentHashMap<>();
+	}
+
+	private TrackManager() {}
 
 	@Autowired
 	public TrackManager(ControllerManager controllerManager) {
@@ -34,45 +41,90 @@ public class TrackManager {
 		this.controllerManager = controllerManager;
 	}
 
-	public TrackManager() {
-		// TODO: make private
-		targetTrackMap = new ConcurrentHashMap<>();
-		trackTargetMultiMap = LinkedListMultimap.create();
-	}
-
-	public void addTarget(RadarTarget target, @Min(2) @Max(10) String callsign) {
-		if (target.getTransponderMode() == TransponderMode.MODE_C) {
-			addCorrelatedTarget(target, callsign);
-		} else if (target.getTransponderMode() == TransponderMode.MODE_S) {
-			addCorrelatedTarget(target, callsign, target.getModeSAddress());
+	public void addTarget(RadarTarget target, @NonNull String callsign) {
+		if (callsign.length() == 0) {
+			addTargetAndAutoCorrelate(target);
 		} else {
-			attemptTargetCorrelation(target);
+			if (target.getTransponderMode() == TransponderMode.MODE_C ||
+					target.getTransponderMode() == TransponderMode.MODE_S) {
+				// TODO: implement mode S correlation methods
+				addCorrelatedTarget(target, callsign);
+			} else {
+				throw new IllegalArgumentException();
+			}
 		}
 	}
 
-	public void attemptTargetCorrelation(RadarTarget target) {
-		if (target.getTransponderMode() == TransponderMode.PRIMARY) {
-			addPrimaryTarget(target);
-		} else {
-			addModeATarget(target);
+	/**
+	 * Add a target and attempt to correlate with a track.
+	 *
+	 * @param target new target to add
+	 */
+	@Synchronized
+	private void addTargetAndAutoCorrelate(RadarTarget target) {
+		throw new UnsupportedOperationException("Auto correlation has not been implemented");
+	}
+
+	/**
+	 * Add a Mode A/C target and correlate with the track with the given callsignHint
+	 *
+	 * @param target new target to add
+	 * @param callsignHint hint from Nxf data source
+	 */
+	@Synchronized
+	private void addCorrelatedTarget(RadarTarget target, String callsignHint) {
+		Track correlatedTrack = trackByCallsignMap.computeIfAbsent(callsignHint, value -> new Track(callsignHint));
+		trackTargetMultiMap.put(correlatedTrack, target);
+		targetTrackMap.put(target, correlatedTrack);
+		trimHistoryRadarTarget(correlatedTrack);
+	}
+
+	/**
+	 * Each add target method must call this method to remove excessive radar targets.
+	 *
+	 * @param track the track to operate on
+	 */
+	@Synchronized
+	private void trimHistoryRadarTarget(Track track) {
+		List<RadarTarget> targetsList = trackTargetMultiMap.get(track);
+		while (targetsList.size() > radarReturnCount) {
+			targetTrackMap.remove(targetsList.remove(0));
 		}
 	}
 
-	public void addCorrelatedTarget(RadarTarget target, String callsign) {
-		throw new UnsupportedOperationException();
-	}
+	/**
+	 * Prune track if the last radar target's return time is over 45 seconds ago.
+	 * TODO: implement coast/suspended track management
+	 */
+	@Synchronized
+	private void pruneStaleTracks() {
+		Instant currentInstant = Instant.now();
 
-	public void addCorrelatedTarget(RadarTarget target, String callsign, @IcaoAddress ByteString address) {
-		throw new UnsupportedOperationException();
+		List<Map.Entry<Track, Collection<RadarTarget>>> toBeRemoved = trackTargetMultiMap.asMap().entrySet().stream()
+				.filter(entry -> {
+					Optional<RadarTarget> newestTarget =
+							entry.getValue().stream().max(Comparator.comparing(RadarTarget::getReturnTime));
+					return newestTarget.filter(radarTarget ->
+							Duration.between(radarTarget.getReturnTime(), currentInstant).getSeconds() > 45
+					).isPresent();
+				})
+				.collect(Collectors.toList());
+
+		toBeRemoved.forEach(trackCollectionEntry -> {
+			targetTrackMap.keySet().removeAll(trackCollectionEntry.getValue());
+			trackTargetMultiMap.removeAll(trackCollectionEntry.getKey());
+			trackByCallsignMap.remove(trackCollectionEntry.getKey().getFlightData().getCallsign());
+		});
 	}
 
 	@Synchronized
-	public void addPrimaryTarget(RadarTarget target) {
+	private void addPrimaryTarget(RadarTarget target) {
 		// find the closest track from the target
 		Optional<RadarTarget> _closestTarget = targetTrackMap.keySet().stream()
 				.min(Comparator.comparingDouble(o -> GeoCalculator.calculateDistance(o, target)));
 
 		// generate a new track by default
+		// TODO: does not work, Track requires unique callsign
 		Track associatedTrack = new Track("PRI");
 
 		if (_closestTarget.isPresent()) {
@@ -89,21 +141,6 @@ public class TrackManager {
 		targetTrackMap.put(target, associatedTrack);
 		trackTargetMultiMap.put(associatedTrack, target);
 
-		List<RadarTarget> targetsList = trackTargetMultiMap.get(associatedTrack);
-		while (targetsList.size() > radarReturnCount) {
-			targetTrackMap.remove(targetsList.remove(0));
-		}
-	}
-
-	public void addModeATarget(RadarTarget target) {
-		Track associatedTrack = new Track("MODE_A");
-	}
-
-	/**
-	 *
-	 */
-	@Synchronized
-	public void pruneStaleTracks() {
-		//trackTargetMultiMap
+		trimHistoryRadarTarget(associatedTrack);
 	}
 }
